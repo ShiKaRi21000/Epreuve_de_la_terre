4db6e8c033fa838e58a60ddcd3fd9245ebbc2c82
GHERBI Kévin <kgherbi21@gmail.com>
2023-12-05 20:39:59 +0100

    modif terre02-03

--- terre02.js
+++ terre02.js
@@ -3,7 +3,6 @@ const { argv } = require('node:process');
 
 argv.forEach ((val, index) => {
   if (index <= 1){
-
   }
   else {
     console.log(`${val}`);
--- terre03.js
+++ terre03.js
@@ -1,12 +1,11 @@
 const { argv } = require('node:process');
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 argv.forEach ((val, index) => {
     if (index <= 1){
     }
     else {
       let correspondance = alphabet.indexOf(val)
-      for (i of alphabet.splice(correspondance))
-        console.log(i)
+        console.log(alphabet.slice(correspondance))
     }
   })
