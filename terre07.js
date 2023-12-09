3cfb107c27bcf06eba6b3813b2b174b600b746b4
GHERBI Kévin <kgherbi21@gmail.com>
2023-12-09 11:12:15 +0100

    Ajout de terre07.js en test

---
+++ terre07.js
@@ -0,0 +1,16 @@
+const mot = process.argv[2]
+let index = ''
+
+if (process.argv[2] === undefined) {
+    console.log('Erreur')
+}
+
+else {
+
+    let i = ''
+    for (i in mot) {
+    }
+    index += i
+    console.log(index)
+}
+
