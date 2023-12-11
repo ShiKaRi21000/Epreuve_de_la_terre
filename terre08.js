d3f08a04c1f71d6c40912b17ea43180fc1403728
GHERBI Kévin <kgherbi21@gmail.com>
2023-12-11 17:27:20 +0100

    Modif de terre08

--- terre08.js
+++ terre08.js
@@ -1,13 +1,13 @@
 /* on gère l'entrée et on vérifie si c'ets un entier*/
 
-if ((parseInt(process.argv[2]) <= 0 ) || (parseInt(process.argv[3]) <= 0)){
+if ((parseInt(process.argv[3]) <= 0)){
     console.log("le calcul doit être fait avec des nombres entiers positifs")
 }
 
 /* on vérifie si un bien un nombre a défault on affiche l'erreur*/
 
 else if (isNaN(process.argv[2])){
-    console.log("Merci d'entré une base valide avec un entier positif")
+    console.log("Merci d'entré une base valide avec un entier")
 }
 
 else if (isNaN(process.argv[3])){
