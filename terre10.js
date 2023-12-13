c6e013b3ee9fdce29dde8b48f4d1e02318e341d5
GHERBI Kévin <kgherbi21@gmail.com>
2023-12-13 06:42:13 +0100

    Modif de terre10.js

--- terre10.js
+++ terre10.js
@@ -1,5 +1,5 @@
 /* premier fonction de l'épreuve qui vérifie si l'entré est un nombre premier.
-on vérifie si c'est supérieur à 1 et sinon son calcul par lui même pour vérifié si le retour est bien 0*/
+on vérifie si c'est supérieur à 1 et sinon si son calcul par lui même uniquement est bien 0 on dit que c'est un nombre premier*/
 
 function nbPremier(nombre) {
     for(var i = 2; i < nombre; i++)
