13ae8646f48efca3dc07f09940e0a57854833ab6
GHERBI Kévin <kgherbi21@gmail.com>
2023-12-14 07:11:55 +0100

    ajout de terre11.js

---
+++ terre11.js
@@ -0,0 +1,24 @@
+let recuperer_heure = parseInt(process.argv[2])
+const recuperer_minute = process.argv[2].slice(process.argv[2].indexOf(":"))
+
+if (recuperer_heure > 11 && recuperer_heure <= 23 && recuperer_heure !== 12){
+    recuperer_heure = recuperer_heure - 12
+    recuperer_heure = recuperer_heure + recuperer_minute
+    console.log(recuperer_heure + "PM")
+}
+
+else if (recuperer_heure <= 11 && recuperer_heure > 0){
+    recuperer_heure = recuperer_heure + recuperer_minute
+    console.log(recuperer_heure + "AM")
+}
+
+else if (recuperer_heure === 0){
+    recuperer_heure = recuperer_heure + recuperer_minute
+    console.log(recuperer_heure + "AM")
+}
+
+else if (recuperer_heure === 12){
+    recuperer_heure = recuperer_heure + recuperer_minute
+    console.log(recuperer_heure + "PM")
+}
+
